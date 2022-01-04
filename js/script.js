1f551c6fde36f2ebf0f9236602c46d34202fa5b4
Roman <downpatay@gmail.com>
2022-01-04 11:02:31 +0200

    reset

--- js/script.js
+++ js/script.js
@@ -13,29 +13,3 @@ wrapper.addEventListener('click', func);
 
 
 
-let validateForms = function(selector, rules, successModal, yaGoal) {
-	new window.JustValidate(selector, {
-		rules: rules,
-		submitHandler: function(form) {
-			let formData = new FormData(form);
-
-			let xhr = new XMLHttpRequest();
-
-			xhr.onreadystatechange = function() {
-				if (xhr.readyState === 4) {
-					if (xhr.status === 200) {
-						console.log('Отправлено');
-					}
-				}
-			}
-
-			xhr.open('POST', 'mail.php', true);
-			xhr.send(formData);
-
-			form.reset();
-
-		}
-	});
-}
-
-validateForms('.form', {email: {required: true, email: true}, name: {required: true}, tel: {required: true} }, '.thanks-popup', 'send goal');
